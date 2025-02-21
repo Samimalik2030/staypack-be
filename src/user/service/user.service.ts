c39cca5924b2efd3410815bd570d0778a6d3af51
sami ullah <msamiullah2030@gmail.com>
2025-02-21 12:38:54 +0500

    module fixed

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -8,20 +8,14 @@ import { forgotPasswordDto } from '../dto/forgotPassword.dto';
 import { TokenService } from 'src/jwt/jwt.service';
 import { OtpService } from 'src/otp/service/otp.service';
 import { OtpType } from 'src/otp/types';
-<<<<<<< HEAD
 import { verifyOTPDto } from '../dto/verify-otp.dto';
 import { Host } from 'src/host/schema/host.mongo';
-=======
-import { AuthUserDto } from '../dto/auth-user.dto';
-import { MessageDto } from '../dto/message.dto';
->>>>>>> 5a898383d37505a4b3a3024fd8befbdd902da96d
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectModel(User.name) private readonly UserModel: Model<User>,
     private readonly jwtTokenService: TokenService,
-<<<<<<< HEAD
     private readonly otpService: OtpService
 
 
@@ -62,43 +56,10 @@ export class UserService {
     console.log(user,'user')
     if (!user) {
       throw new NotFoundException('invalid credantial')
-=======
-    private readonly otpService: OtpService,
-  ) {}
-
-  async findByEmail(email: string): Promise<User | null> {
-    const user = await this.UserModel.findOne({
-      email: email,
-    });
-    return user;
-  }
-
-  async signUp(data: SignUpDto): Promise<AuthUserDto> {
-    const createdUser = await this.UserModel.create(data);
-    const token = await this.jwtTokenService.generateToken({
-      email: createdUser.email,
-      id: createdUser._id,
-      role: createdUser.role,
-    });
-    return {
-      user: createdUser,
-      token: token,
-    };
-  }
-
-  async signIn(data: SignInDto): Promise<AuthUserDto> {
-    const user = await this.UserModel.findOne({
-      email: data.email,
-      password: data.password,
-    });
-    if (!user) {
-      throw new NotFoundException('Invalid credentials');
->>>>>>> 5a898383d37505a4b3a3024fd8befbdd902da96d
     }
     const token = await this.jwtTokenService.generateToken({
       email: user.email,
       id: user._id,
-<<<<<<< HEAD
       role: user.role
     })
     return {
@@ -113,28 +74,11 @@ export class UserService {
     })
     if (!user) {
       throw new NotFoundException('user not found')
-=======
-      role: user.role,
-    });
-    return {
-      user: user,
-      token: token,
-    };
-  }
-
-  async forgotPassword(data: forgotPasswordDto): Promise<MessageDto> {
-    const user = await this.UserModel.findOne({
-      email: data.email,
-    });
-    if (!user) {
-      throw new NotFoundException('user not found');
->>>>>>> 5a898383d37505a4b3a3024fd8befbdd902da96d
     }
     const otp = Math.floor(100000 + Math.random() * 900000);
     await this.otpService.storeOtp({
       otp: otp,
       type: OtpType.FORGOT_PASSWORD,
-<<<<<<< HEAD
       user: user
     })
     return {
@@ -191,30 +135,4 @@ async deleteHost(id:string){
 //   return updatedUser
 // }
 }
-=======
-      user: user,
-    });
-    return {
-      message: `Otp sent to your email.Please use this otp ${otp}`,
-    };
-  }
-
-  async updatePassword(
-    id: string,
-    password: string,
-    confirmPassword: string,
-  ): Promise<User | null> {
-    const updatedPassword = await this.UserModel.findByIdAndUpdate(
-      id,
-      {
-        password: password,
-        confirmPassword: confirmPassword,
-      },
-      {
-        returnDocument: 'after',
-      },
-    );
-    return updatedPassword;
-  }
->>>>>>> 5a898383d37505a4b3a3024fd8befbdd902da96d
 }
